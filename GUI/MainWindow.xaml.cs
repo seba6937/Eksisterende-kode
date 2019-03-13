886801893afaae3e81065ce5c729e47fe6fbcc60
Kenni Claus Holm <kenn5363@edu.eal.dk>
2019-03-13 19:18:41 +0100

    Hej

---
+++ GUI/MainWindow.xaml.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace GUI
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void AktuelleFejlRapporter_Click(object sender, RoutedEventArgs e)
+        {
+            Windows.AktuelleFejlRapporter window = new Windows.AktuelleFejlRapporter();
+            window.Show();
+            this.Close();
+        }
+
+        private void OpretFejl_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void ÆndreFejl_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void VisGamleRapporter_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void RapporterEkstraInfo_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
---
+++ GUI/Windows/AktuelleFejlRapporter.xaml.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace GUI.Windows
+{
+    /// <summary>
+    /// Interaction logic for AktuelleFejlRapporter.xaml
+    /// </summary>
+    public partial class AktuelleFejlRapporter : Window
+    {
+        public AktuelleFejlRapporter()
+        {
+            InitializeComponent();
+        }
+
+        private void VisAlle_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void VisBryst_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void VisRyg_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void VisMave_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void VisSpinning_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void VisBen_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void VisArme_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Tilbage_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
